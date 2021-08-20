6d9a6018c1cb270e77e9ecaeb43e18573afbe698
maroberto <63175966+maroberto@users.noreply.github.com>
2021-08-20 17:59:59 -0300

    Finalização de Aula

--- App.js
+++ App.js
@@ -1,21 +1,151 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  Image, 
+  TextInput, 
+  TouchableOpacity, 
+  Platform,
+  KeyboardAvoidingView,
+  Keyboard,
+  TouchableWithoutFeedback, 
+} from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+
 
 export default function App() {
   return (
+
+    <KeyboardAvoidingView
+      behavior= {Platform.os === 'ios' ? 'padding' : 'height'}
+    >
+    <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss() }>  
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="#fff" translucent={false} />
+      <Image
+        source={require('./src/assets/logo.png/')}
+        style={styles.logo}
+      />
+
+      <TextInput
+        placeholder="Celular, username ou email"
+        style={styles.Input}
+      />
+      <TextInput
+        placeholder="Sua senha"
+        style={styles.Input}
+      />
+
+      <View style={styles.forgoutContainer}>
+        <TouchableOpacity>
+          <Text style={styles.forgoutText}>Esqueceu sua senha</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.loginButton} >
+        <Text style={styles.loginText}>Acessar</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.facebookContainer}>
+        <FontAwesome5 name="facebook" size={25} color="#399fff"/>
+        <Text style={styles.facebookText}>Continue como Rafael</Text>
+      </TouchableOpacity>
+
+      <View style={styles.divisor}>
+        <View style={styles.divisorLine}></View>
+        <Text style={{ marginHorizontal: '3%' }}>OU</Text>
+        <View style={styles.divisorLine}></View>
+      </View>
+
+      <View style={styles.sigupContainer}>
+        <Text style={styles.sigupText}>Não possui uma conta</Text>
+        <TouchableOpacity>
+          <Text style={styles.sigupButton}>Casdastre-se</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
+    </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo: {
+    marginTop: Platform.os === 'android' ? '13%' : '20%',
+    marginBottom: Platform.os === 'android' ? '13%' : '15%',
+  },
+  Input: {
+    width: '90%',
+    height: 42,
+    backgroundColor: '#f4f3f3',
+    marginBottom: 20,
+    padding: 8,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#E0E0E0',
+  },
+  forgoutContainer:{
+    width: '90%',
+    alignItems: 'flex-end',
+  },
+  forgoutText:{
+    color: '#399fff'
+  },
+  loginButton:{
+    marginTop: '5%',
+    backgroundColor: '#399fff',
+    width: '90%',
+    height: 42,
+    justifyContent: 'center',
+    alignItems:'center',
+    borderRadius: 5,
+  },
+  loginText:{
+    color: '#fff',
+    fontSize: 17,
+  },
+  facebookContainer:{
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop:'10%',
+  },
+  facebookText:{
+    color: '#399fff',
+    padding: 8,
+    fontSize: 15,
+  },
+  divisor:{
+    marginTop: '5%',
+    flexDirection: 'row',
+    width: '90%',
+    alignItems:'center',
+    justifyContent: 'center',
+  },
+  divisorLine:{
+    width: '45%',
+    height: 2,
+    backgroundColor: '#EFEDED',
+    borderRadius: 5,
+  },
+  sigupContainer:{
+    flexDirection: 'row',
+    marginTop: '10%',
+  },
+  sigupText:{
+    color: '#c4c4c4',
+    paddingRight: 5,
+  },
+  sigupButton:{
+    color: '#399fff',
+    fontWeight: 'bold',
+  }
 });
---
+++ README.md
@@ -0,0 +1,18 @@
+# React Native Tela clone do Instagram
+
+## Resultado da ultima aula do de React Native. 
+Esta aplicação simples é resultado dos meus estudos em React Native, uma reprodução da tela do Instagram. E nesta aula, usamos o Figma, para fazer uma prévia da interface.
+
+
+_____
+![](images/img22.jpg)
+_____
+ Designer feito no Figma.
+![](images/img1.png)
+
+_____
+
+
+## Licença:
+
+[GNU General Public License v3.0](https://github.com/maroberto/React_Native_Calculadora/blob/master/LICENSE)
